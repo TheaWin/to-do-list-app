848d05805a3b5751df61325c0b901badca44cd0c
Thea Win <winwinkhaing.wk.14@gmail.com>
2023-11-29 22:29:04 +0800

    add-new-item
    added new item to the list of items

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* //COMMENTS ARE IN JAVASCRIPT */
+
+function newItem () {
+    // let li = document.createElement("li");
+    let li = $('<li></li>');
+    //let inputValue = document.getElementById("input").value;
+    //let text = document.createTextNode(inputValue);
+    let inputValue = $('#input').val();
+    //li.appendChild(text); 
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert ('You must write something!');
+    } else {
+        //let list = document.querySelector('#list');
+        //list.appendChild(li);
+        $('#list').append(li);
+    }
+}
